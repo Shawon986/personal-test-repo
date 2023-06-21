00092089191971a8b66104d641f783eb655ab6fe
Shawon Hossain <mdmonowerhossain986@gmail.com>
2023-06-21 16:05:07 +0600

    add refresh token

--- Node JS app/server.js
+++ Node JS app/server.js
@@ -11,191 +11,263 @@ app.use(bodyParser.json());
 //MONGODB Connection
 
 const uri = process.env.DB_URI;
-mongoose.connect(uri,{useNewUrlParser:true})
-.then(()=>console.log("Database Is Connected"))
-.catch(()=>console.log("Database Is Not Connected"))
+mongoose
+  .connect(uri, { useNewUrlParser: true })
+  .then(() => console.log("Database Is Connected"))
+  .catch(() => console.log("Database Is Not Connected"));
 
 //Schema
 
-const customerSchema =new mongoose.Schema({
-    name:String,
-    address:Object,
-    email:String,
-    password:String,
+const customerSchema = new mongoose.Schema({
+  name: String,
+  address: Object,
+  email: String,
+  password: String,
 });
 
-const Customer =  mongoose.model("Customer",customerSchema);
+const Customer = mongoose.model("Customer", customerSchema);
 
 //! Check connection
 
-app.get("/",(req,res)=>{
-    res.json({message:"Connection Ok Bro!!!"})
+app.get("/", (req, res) => {
+  res.json({ message: "Connection Ok Bro!!!" });
 });
 
 //! Create Customer
 
-app.post("/customers",async(req,res)=>{
-    try {
-        //Password Hashing
+app.post("/customers", async (req, res) => {
+  try {
+    //Password Hashing
 
-        const hash =await bcrypt.hash(req.body.password,10); 
-        const password = hash;
-        
-        const customerObject ={
-            name:req.body.name,
-            address:req.body.address,
-            email:req.body.email,
-            password:password,
-        }
-        const customer = new Customer(customerObject);
-        await customer.save();
-        res.status(201).json(customer);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message:"Something went wrong with the server!!!"})
-    }
-   
+    const hash = await bcrypt.hash(req.body.password, 10);
+    const password = hash;
+
+    const customerObject = {
+      name: req.body.name,
+      address: req.body.address,
+      email: req.body.email,
+      password: password,
+    };
+    const customer = new Customer(customerObject);
+    await customer.save();
+    res.status(201).json(customer);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Something went wrong with the server!!!" });
+  }
 });
 
 //! Login Customer BY EMAIL AND PASSWORD
 
-app.post("/customers/login",async(req,res)=>{
-    try {
-        const {email,password}=req.body
-        const customer =await Customer.findOne({email:email});
-        if(!customer){
-        res.status(404).json({message:"Customer not found!!!"})
-        }else{
-            const validPassword=await bcrypt.compare(password,customer.password);
-            if(!validPassword){
-               res.status(401).json({message:"Customer unauthorized"})
-            }else{
-               const accessToken = jwt.sign({email:customer.email,id:customer._id},process.env.JWT_SECRET)
-               customerObject =customer.toJSON();
-               customerObject.jwtToken = accessToken;
-               res.json(customerObject);
-            }
+app.post("/customers/login", async (req, res) => {
+  try {
+    const { email, password, type, refreshToken } = req.body;
+    if (!type) {
+      res.status(404).json({ message: "Type is not defined!!!" });
+    } else {
+      if (type == "email") {
+        const customer = await Customer.findOne({ email: email });
+        if (!customer) {
+          res.status(404).json({ message: "Customer not found!!!" });
+        } else {
+          const validPassword = await bcrypt.compare(
+            password,
+            customer.password
+          );
+          if (!validPassword) {
+            res.status(401).json({ message: "Customer unauthorized" });
+          } else {
+            const accessToken = jwt.sign(
+              { email: customer.email, id: customer._id },
+              process.env.JWT_SECRET,
+              { expiresIn: "1m" }
+            );
+
+            const refreshToken = jwt.sign(
+              { email: customer.email, id: customer._id },
+              process.env.JWT_SECRET,
+              { expiresIn: "2m" }
+            );
+
+            customerObject = customer.toJSON();
+
+            customerObject.accessToken = accessToken;
+            customerObject.refreshToken = refreshToken;
+
+            res.json(customerObject);
+          }
         }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message:"Something went wrong with the server!!!"})
+      } else {
+        if (!refreshToken) {
+          res.status(404).json({ message: "refreshToken is not defined!!!" });
+        } else {
+          jwt.verify(
+            refreshToken,
+            process.env.JWT_SECRET,
+            async (err, payload) => {
+              if (err) {
+                res.status(401).json({ message: "Customer Unauthorized!!!" });
+              } else {
+                const id = payload.id;
+                const customer = await Customer.findById(id);
+                if (!customer) {
+                  res.status(404).json({ message: "Customer not found!!!" }); 
+                } else {
+                  const accessToken = jwt.sign(
+                    { email: customer.email, id: customer._id }, 
+                    process.env.JWT_SECRET,
+                    { expiresIn: "1m" }
+                  );
+
+                  const refreshToken = jwt.sign(
+                    { email: customer.email, id: customer._id },
+                    process.env.JWT_SECRET,
+                    { expiresIn: "2m" }
+                  );
+
+                  customerObject = customer.toJSON();
+
+                  customerObject.accessToken = accessToken; 
+                  customerObject.refreshToken = refreshToken;
+
+                  res.json(customerObject);
+                }
+              }
+            }
+          );
+        }
+      }
     }
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Something went wrong with the server!!!" });
+  }
 });
 
-//! Middleware to Authenticate JWT ACCESS TOKEN 
+//! Middleware to Authenticate JWT ACCESS TOKEN
 
-const authenticateToken = (req,res,next)=>{
-    const authHeader = req.headers.authorization;
-    const token = authHeader && authHeader.split(" ")[1];
-    if(!token){
-        res.status(401).json({message:"Customer Unauthorized!!!"})
-        return
-    }else{
-        jwt.verify(token,process.env.JWT_SECRET,(err,customer)=>{
-            if(err){
-                res.status(401).json({message:"Customer Unauthorized!!!"})
-            }else{
-                req.customer=customer;
-                next()
-            }
-        });
-        
-    }
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    res.status(401).json({ message: "Customer Unauthorized!!!" });
+    return;
+  } else {
+    jwt.verify(token, process.env.JWT_SECRET, (err, customer) => {
+      if (err) {
+        res.status(401).json({ message: "Customer Unauthorized!!!" });
+      } else {
+        req.customer = customer;
+        next();
+      }
+    });
+  }
 };
 
 //! Get a Customer Profile
 
-app.get("/customers/profile",authenticateToken, async(req,res)=>{
-    try {
-        const id = req.customer.id;
-        const customer = await Customer.findById(id);
-        if(!customer){
-            res.status(404).json({message:"Customer not found!!!"})
-        }else{
-            res.json(customer)
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message:"Something went wrong with the server!!!"})
+app.get("/customers/profile", authenticateToken, async (req, res) => {
+  try {
+    const id = req.customer.id;
+    const customer = await Customer.findById(id);
+    if (!customer) {
+      res.status(404).json({ message: "Customer not found!!!" });
+    } else {
+      res.json(customer);
     }
-
-})
-
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Something went wrong with the server!!!" });
+  }
+});
 
 //! Get all Customers
 
-app.get("/customers",async(req,res)=>{
-    try {
-        const customer= await Customer.find({});
-        res.json(customer)
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message:"Something went wrong with the server!!!"})
-    }
+app.get("/customers", async (req, res) => {
+  try {
+    const customer = await Customer.find({});
+    res.json(customer);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Something went wrong with the server!!!" });
+  }
 });
 
 //! Get a Customer by id
 
-app.get("/customers/:id",async(req,res)=>{
-    try {
-        const id = req.params.id;
-        const customer = await Customer.findById(id);
-        if(!customer){
-            res.status(404).json({message:"Customer not found!!!"})
-        }else{
-            res.json(customer)
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message:"Something went wrong with the server!!!"})
+app.get("/customers/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const customer = await Customer.findById(id);
+    if (!customer) {
+      res.status(404).json({ message: "Customer not found!!!" });
+    } else {
+      res.json(customer);
     }
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Something went wrong with the server!!!" });
+  }
 });
 
 //! Update a Customer by id
 
-app.put("/customers/:id",async(req,res)=>{
-    try {
-        const id = req.params.id;
-        const customer = await Customer.findByIdAndUpdate(id,req.body,{new:true});
-        if(!customer){
-            res.status(404).json({message:"Customer not found!!!"})
-        }else{
-            const hash =await bcrypt.hash(req.body.password,10); 
-            const password = hash;
-            customer.name=req.body.name,
-            customer.address=req.body.address,
-            customer.email=req.body.email,
-            customer.password=password,
-            res.json(customer)
-            await customer.save();
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message:"Something went wrong with the server!!!"})
+app.put("/customers/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const customer = await Customer.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!customer) {
+      res.status(404).json({ message: "Customer not found!!!" });
+    } else {
+      const hash = await bcrypt.hash(req.body.password, 10);
+      const password = hash;
+      (customer.name = req.body.name),
+        (customer.address = req.body.address),
+        (customer.email = req.body.email),
+        (customer.password = password),
+        res.json(customer);
+      await customer.save();
     }
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Something went wrong with the server!!!" });
+  }
 });
 
 //! Delete a Customer by id
 
-app.delete("/customers/:id",async(req,res)=>{
-    try {
-        const id = req.params.id;
-        const customer = await Customer.findByIdAndDelete(id);
-        if(!customer){
-            res.status(404).json({message:"Customer not found!!!"})
-        }else{
-            res.json(customer)
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message:"Something went wrong with the server!!!"})
+app.delete("/customers/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const customer = await Customer.findByIdAndDelete(id);
+    if (!customer) {
+      res.status(404).json({ message: "Customer not found!!!" });
+    } else {
+      res.json(customer);
     }
-})
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Something went wrong with the server!!!" });
+  }
+});
 
-
-const port = process.env.PORT 
-app.listen(port,()=>{
-    console.log(`App is running on port ${port}`) 
-}) 
-
-
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
