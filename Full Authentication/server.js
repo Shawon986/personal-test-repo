ce16b65a8ee00dd055f7bf61d95afe7877cd5c28
Shawon Hossain <mdmonowerhossain986@gmail.com>
2023-06-22 20:22:17 +0600

    Database connect

--- Full Authentication/server.js
+++ Full Authentication/server.js
@@ -4,9 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv").config();
 const app = express();
+const mongoose = require("mongoose");
 app.use(bodyParser.json());
-
-
+const uri = process.env.DB_URI;
+mongoose.connect(uri,{useNewUrlParser:true})
+.then(()=>console.log("Database is connected"))
+.catch((error)=>console.error("Database is not connected"))
 
 
 
