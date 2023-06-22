261705c1db7e2025b3f1891e858952a1968461c8
Shawon Hossain <mdmonowerhossain986@gmail.com>
2023-06-22 21:12:07 +0600

    password hash

--- Full Authentication/server.js
+++ Full Authentication/server.js
@@ -5,13 +5,55 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv").config();
 const app = express();
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 app.use(bodyParser.json());
 const uri = process.env.DB_URI;
 mongoose.connect(uri,{useNewUrlParser:true})
 .then(()=>console.log("Database is connected"))
 .catch((error)=>console.error("Database is not connected"))
 
+const packageSchema = new mongoose.Schema({
+    name:String,
+    from:String,
+    to:String,
+    ticketPrice:String,
+    email:String,
+    password:String
+});
 
+const Package = mongoose.model("Package",packageSchema)
+
+//! Check connection
+
+app.get("/",(req,res)=>{
+    res.json({message:"Welcome !!!"})
+});
+
+//! Create Customer tour package
+
+app.post("/packages",async(req,res)=>{
+    try {
+        const hash = await bcrypt.hash(req.body.password,10);
+        const password = hash;
+        const packageObject ={
+            name:req.body.name,
+            from:req.body.from,
+            to:req.body.to,
+            ticketPrice:req.body.ticketPrice,
+            email:req.body.email,
+            password:password
+        }
+        const package = new Package(packageObject) ;
+        res.status(201).json(package);
+        await package.save();  
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({message:"Something went wrong!!!"})
+
+    }
+
+})
 
 
 
