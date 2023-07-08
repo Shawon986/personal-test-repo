90dca3a3a06e24e0ed937b6920d4c172a52659e3
Shawon Hossain <mdmonowerhossain986@gmail.com>
2023-07-08 19:18:35 +0600

    accessToken generate and password hashing

--- Express js app/server.js
+++ Express js app/server.js
@@ -2,6 +2,8 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const dotenv = require("dotenv").config()
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const app = express()
 app.use(bodyParser.json())
 
@@ -30,11 +32,13 @@ app.get("/",(req,res)=>{
 //! Api to create a user
 app.post("/users",async(req,res)=>{
     try {
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(req.body.password,salt) 
         const userObject={
             name:req.body.name,
             age:req.body.age,
             email:req.body.email,
-            password:req.body.password
+            password:hashedPassword
         }
         const user = User(userObject) 
         await user.save()
@@ -44,6 +48,31 @@ app.post("/users",async(req,res)=>{
         console.error(error)
     }
     
+})
+
+//! Api to login a user(email/password)
+app.post("/users/login",async(req,res)=>{
+    try {
+        const {email,password}=req.body
+        const user =await User.findOne({email:email})
+        if(!user){
+            res.status(404).json({message:"User not found !!!"})
+        }else{
+            const validPassword= await bcrypt.compare(password,user.password)
+            if(!validPassword){
+                res.status(401).json({message:"User unauthorized"})
+            }else{
+                const accessToken = jwt.sign({email:user.email,id:user._id},process.env.JWT_SECRET)
+                userObject=user.toJSON()
+                userObject.accessToken= accessToken
+                res.json(userObject)
+            }
+        }
+    } catch (error) {
+        res.status(400).json({message:"Something went wrong with the server"})
+        console.error(error.message)
+    }
+
 })
 
 //! Api to get all users
@@ -72,11 +101,15 @@ app.get("/users/:id",async(req,res)=>{
 //! Api to update a user
 app.put("/users/:id",async(req,res)=>{
     try {
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(req.body.password,salt) 
         const id = req.params.id
         const user =await User.findByIdAndUpdate(id,req.body,{new:true})
         if(!user){
             res.status(404).json({message:"User not found !!!"})
         }else{
+            user.password=hashedPassword
+            await user.save()
             res.json(user)
         }
     } catch (error) {
