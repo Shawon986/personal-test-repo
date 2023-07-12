109728619c110ab4b25380ddde8ee159d444208a
Shawon Hossain <mdmonowerhossain986@gmail.com>
2023-07-13 02:03:46 +0600

    password hashing and create a user

--- Express js Task manager project/server.js
+++ Express js Task manager project/server.js
@@ -2,6 +2,7 @@ const express =require("express")
 const bodyParser = require("body-parser")
 const dotenv = require("dotenv").config()
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
 const app = express()
 app.use(bodyParser.json())
 
@@ -26,10 +27,28 @@ const User = mongoose.model("User",userSchema)
 
 //! check connection
 app.get("/",(req,res)=>{
-    res.json({message:"Welcome to Task Manager"})
+    res.status(400).json({message:"Welcome to Task Manager"})
 })
 
 //! create a user
+app.post("/users",async(req,res)=>{
+    try {
+    const hashedPassword= await bcrypt.hash(req.body.password,10)
+    
+    userObject={
+        name:req.body.name,
+        email:req.body.email,
+        password:hashedPassword
+    }
+    const user = new User(userObject)
+    await user.save()
+    res.status(201).json(user)
+
+    } catch (error) {
+        res.json({message:"Something went wrong"})
+        console.error(error)
+    }
+})
 
 
 
