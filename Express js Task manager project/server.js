497cbd0cb6a6e66b94c06d504273b85bbe4e0a96
Shawon Hossain <mdmonowerhossain986@gmail.com>
2023-07-13 00:59:14 +0600

    monggose connect and schema create

--- Express js Task manager project/server.js
+++ Express js Task manager project/server.js
@@ -1,8 +1,35 @@
 const express =require("express")
 const bodyParser = require("body-parser")
 const dotenv = require("dotenv").config()
+const mongoose = require("mongoose")
 const app = express()
 app.use(bodyParser.json())
+
+const uri = process.env.DB_URI
+mongoose.connect(uri,{useNewUrlParser:true})
+.then(()=>console.log("DB is Connected"))
+.catch((error)=>console.error("DB is not connected"))
+
+const userSchema= new mongoose.Schema({
+    name : {
+        type:String,
+    },
+    email : {
+        type: String,
+    },
+    password : {
+        type: String
+    }
+})
+
+const User = mongoose.model("User",userSchema)
+
+//! check connection
+app.get("/",(req,res)=>{
+    res.json({message:"Welcome to Task Manager"})
+})
+
+//! create a user
 
 
 
@@ -11,3 +38,4 @@ const port = process.env.PORT
 app.listen(port,()=>{
     console.log(`app is running on port ${port}`)
 })
+
