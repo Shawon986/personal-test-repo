eb921c1d5fa5af28d2803e874df11c799f674133
Shawon Hossain <mdmonowerhossain986@gmail.com>
2023-07-22 23:59:06 +0600

    task schema done

--- Express js Task manager project/routes/api/tasks.js
+++ Express js Task manager project/routes/api/tasks.js
@@ -1,1 +1,24 @@
- 
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+    title: {
+      type: String,
+    },
+    status: {
+      type: String,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    description: {
+        type: String,
+    },
+    expiresAt:{
+        type: Date
+    }
+  },{
+      timestamps:true
+  });
+
+  module.exports =  Task = mongoose.model("Task", taskSchema);
